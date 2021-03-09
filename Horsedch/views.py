04943bb8e371d281a64674ff9dabb6d36b28a629
MuhammadUsman-DevOps <usmanamin000@gmail.com>
2021-03-09 19:57:56 +0500

    90% Front-end Designed

--- Horsedch/views.py
+++ Horsedch/views.py
@@ -7,3 +7,7 @@ def index_view(request):
 
 def sign_up_view(request):
     return render(request, template_name="authentication/signup_using_email.html")
+
+
+def test_view(request):
+    return render(request, template_name="take_on_rent/checkout.html")
