df32e2ea6a565bd4400970b7d1ca6795dff570e7
MuhammadUsman-DevOps <usmanamin000@gmail.com>
2021-04-05 23:30:17 +0500

    Landing website completed

--- Horsedch/models.py
+++ Horsedch/models.py
@@ -2,6 +2,102 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 
+class HeroSection(models.Model):
+    site_title = models.CharField(max_length=200, help_text="Enter website name/title.")
+    site_description = models.TextField(max_length=300, help_text="Enter website description/tag line.")
+    is_active = models.BooleanField(default=False,
+                                    help_text="Uncheck to hide this title from website instead of deleting.")
+
+    def __str__(self):
+        return self.site_title
+
+    class Meta:
+        verbose_name = "Hero Section"
+        verbose_name_plural = "Hero Section"
+
+
+class Box(models.Model):
+    box_title = models.CharField(max_length=50, help_text="Enter box name/title.")
+    box_description = models.TextField(max_length=100, help_text="Enter box description/tag line.")
+    is_active = models.BooleanField(default=False,
+                                    help_text="Uncheck to hide this title from website instead of deleting.")
+
+    def __str__(self):
+        return self.box_title
+
+    class Meta:
+        verbose_name = "Boxes Section"
+        verbose_name_plural = "Boxes Section"
+
+
+class AboutUs(models.Model):
+    heading = models.CharField(max_length=100, help_text="Enter about us heading.")
+    sub_heading = models.CharField(max_length=150, help_text="Enter about us sub-heading.")
+    description = models.TextField(max_length=1000, help_text="Enter about us description.")
+    is_active = models.BooleanField(default=False,
+                                    help_text="Uncheck to hide this title from website instead of deleting.")
+
+    def __str__(self):
+        return self.heading
+
+    class Meta:
+        verbose_name = "About us Section"
+        verbose_name_plural = "About us Section"
+
+
+class Team(models.Model):
+    person_img = models.ImageField(upload_to="images/team", null=True, blank=True,
+                                   help_text="Please select an image of the person.")
+    full_name = models.CharField(max_length=50, help_text="Enter person name.")
+    designation = models.CharField(max_length=50, help_text="Enter person's designation.")
+    facebook_link = models.URLField(max_length=200, help_text="Please enter facebook profile/page URL.")
+    instagram_link = models.URLField(max_length=200, help_text="Please enter instagram profile URL.")
+    email_address = models.EmailField(help_text="Please enter person's email address")
+
+    def __str__(self):
+        return self.designation
+
+    class Meta:
+        verbose_name = "Teams Section"
+        verbose_name_plural = "Teams Section"
+
+
+class GeneralFAQs(models.Model):
+    question = models.CharField(max_length=100, help_text="Enter question here.")
+    answer = models.TextField(max_length=300, help_text="Enter question here.")
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = "General FAQs"
+        verbose_name_plural = "General FAQs"
+
+
+class HowToRentFAQs(models.Model):
+    question = models.CharField(max_length=100, help_text="Enter question here.")
+    answer = models.TextField(max_length=300, help_text="Enter question here.")
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = "How to rent FAQs"
+        verbose_name_plural = "How to rent FAQs"
+
+
+class HowToListFAQs(models.Model):
+    question = models.CharField(max_length=100, help_text="Enter question here.")
+    answer = models.TextField(max_length=300, help_text="Enter question here.")
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = "How to list FAQs"
+        verbose_name_plural = "How to list FAQs"
+
+
 class HowItWork(models.Model):
     picture = models.ImageField(upload_to="images/how_it_work", null=True, blank=True,
                                 help_text="Please select an image/icon.")
@@ -56,7 +152,7 @@ class ContactInformation(models.Model):
     email_address = models.EmailField(max_length=200, null=True, blank=True,
                                       help_text="Please enter company email address here.")
     phone_number = models.CharField(max_length=50, null=True, blank=True,
-                                    help_text="Please enter comapny phone number here.")
+                                    help_text="Please enter company phone number here.")
 
     def __str__(self):
         return self.building_name
--- Horsedch/views.py
+++ Horsedch/views.py
@@ -1,19 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-from Horsedch.models import HowItWork, WhyHorsedCh, ContactInformation, Condition, DataPolicy, FairPlay, Imprint
+from Horsedch.models import HowItWork, WhyHorsedCh, ContactInformation, Condition, DataPolicy, FairPlay, Imprint, \
+    HeroSection, Box, AboutUs, GeneralFAQs, HowToRentFAQs, HowToListFAQs, SocialLinks, Team
 
 
 def index_view(request):
+    hero_section = HeroSection.objects.latest('site_title')
+    box = Box.objects.all()
+    about_us = AboutUs.objects.latest('heading')
+    team = Team.objects.all()
+    general_faqs = GeneralFAQs.objects.all()
+    how_to_rent_faqs = HowToRentFAQs.objects.all()
+    how_to_list_faqs = HowToListFAQs.objects.all()
+    social_links = SocialLinks.objects.latest('facebook')
     how_it_works = HowItWork.objects.all()
-    why_horsed_ch = WhyHorsedCh.objects.all()
     company_contact = ContactInformation.objects.latest('building_name')
     context = {
         'how_it_works': how_it_works,
-        'why_horsed_ch': why_horsed_ch,
         'company_contact': company_contact,
+        'hero_section': hero_section,
+        'boxes': box,
+        'about_us': about_us,
+        'team': team,
+        'general_faqs': general_faqs,
+        'how_to_rent_faqs': how_to_rent_faqs,
+        'how_to_list_faqs': how_to_list_faqs,
+        'social_links': social_links,
 
     }
-    return render(request, template_name="index.html", context=context)
+    return render(request, template_name="site_pages/index.html", context=context)
 
 
 def sign_up_view(request):
@@ -107,3 +123,15 @@ def single_product_details(request):
 
 def test(request):
     return render(request, template_name="site_pages/index.html")
+
+
+def login(request):
+    if request.method == "POST":
+        if request.POST.get("email_address") == "seller@horsedch.com":
+            return redirect('/my/account')
+        elif request.POST.get("email_address") == "buyer@horsedch.com":
+            return redirect('/my/account')
+        else:
+            messages.error(request, "Oops! Invalid username or password")
+
+    return render(request, template_name="authentication/auth_login.html")
